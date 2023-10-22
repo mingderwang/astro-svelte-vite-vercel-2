4f4cd5bc271245f87e14af6397128e2cdc3bdd50
Ming-der Wang <mingderwang@gmail.com>
2023-10-22 14:11:26 +0800

    login UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import svelte from '@astrojs/svelte';
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
   integrations: [svelte()]
 });
